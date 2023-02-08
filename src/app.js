53e65da7db12ad97a3e82b4d01d3cfb440255822
www.francisjjba@gmail.com <francisjjba@gmail.com>
2023-02-08 16:31:54 +0530

    react setup 08-02-2023

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 // eslint-disable-next-line import/prefer-default-export
-export function counter() { 
-    return hello 
+export function counter() {
+  return hello;
 }
 
 // modules.export={
-//     counter, 
+//     counter,
 // };
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './public/index.html',
+    filename: 'index.html',
+  })],
+
+};
